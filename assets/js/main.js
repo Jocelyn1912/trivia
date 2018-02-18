25150777a45fe142afb2048f4afa440e4bcbb025
pamela <pamela@MacBook-Pro-de-pamela.local>
2018-02-18 17:58:39 -0300

    preguntas con selección de respuestas y estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,36 @@
-$('#start').click(function(event) {
+$('#start, .next').click(function(event) {
   event.target;
   getTrivia();
 })
 
 function getTrivia() {
-  fetch(`https://opentdb.com/api.php?amount=10`)
+  fetch(`https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=multiple`)
+  // boolean para true/false
     .then(function(response) {
     // Turns the the JSON into a JS object
       return response.json();
     })
-    .then(function(categories) {
-      const question = categories.results[0].question;
-      console.log(categories);
-      $('#container').append(question);
-    });
+    .then(function(data) {
+      $('#intro').remove();
+      const question = data.results[0].question;
+      const correct = data.results[0].correct_answer;
+      const incorrect = data.results[0].incorrect_answers;
+      
+      $('#container').append('<div class="trivias"><h3>' + question + '</h3><button value="correct" class="option">' + correct + '</button><button value="incorrect" class="option">' + incorrect[0] + '</button><button value="incorrect" class="option">' + incorrect[1] + '</button><button value="incorrect" class="option">' + incorrect[2] + '</button><button class="next">Next Question</button></div>');
+
+      // función seleccion
+      $('.option').click(function() {
+        const select = $(this).val();
+        if (select == "correct") {
+          $(this).addClass("correct");
+          $('.trivias').append('<h3>¡Correct!</h3>')
+        }else{
+          $(this).addClass("incorrect");
+          $('.trivias').append('<h3>¡Incorrect!</h3>')
+        }
+      });
+
+
+    });//data
 }
 
