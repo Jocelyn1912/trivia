c988ae42f400e5247fae76ecfc7737b531909301
Jocelyn1912 <toty_2410@hotmail.com>
2018-02-18 23:08:45 -0300

    Mostrando ressultados

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,22 +14,26 @@ $('#start, #next').click(function() {
       const arrayDatos = datos.sort();
 
       $('#trivias').append('<h3>' + arrayDatos[0].question + '</h3><button value="correct" class="option">' + arrayDatos[0].correct_answer + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[0] + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[1] + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[2] + '</button>');
-
-      // función seleccion
-      $('.option').click(function() {
-        const select = $(this).val();
-        if (select === 'correct') {
-          $(this).addClass('correct');
-          $('#next').after('<h3 class="answer">¡Correct! </h3>');
-          counterCorrect++;
-          console.log(counterCorrect);
-        } else {
-          $(this).addClass('incorrect');
-          $('#next').after('<h3 class="answer">¡Incorrect! The correct answer is: "' + data.results[0].correct_answer + '" </h3>');
-          counterIncorrect++;
-          console.log(counterIncorrect);
-        }
-      });
+      let sumCounter = counterCorrect+counterIncorrect;
+      if (sumCounter < 10) {
+        // función seleccion
+        $('.option').click(function() {
+          const select = $(this).val();
+          if (select === 'correct') {
+            $(this).addClass('correct');
+            $('#next').after('<h3 class="answer">¡Correct! </h3>');
+            counterCorrect++;
+          } else {
+            $(this).addClass('incorrect');
+            $('#next').after('<h3 class="answer">¡Incorrect! The correct answer is: "' + data.results[0].correct_answer + '" </h3>');
+            counterIncorrect++;
+          }
+        });
+      } if (sumCounter === 10) {
+        $('#trivias').empty();
+        $('#next').addClass('hidden');
+        $('#trivias').append('<h3 class="end"> Obtuviste ' + counterCorrect + ' respuestas correctas y ' + counterIncorrect + ' respuestas incorrectas. </h3>')
+      }
     });// data
 });
 
