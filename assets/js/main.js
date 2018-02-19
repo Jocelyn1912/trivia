439c8f082edba26ffb7c740fc72a60fd9fac2db7
Jocelyn1912 <toty_2410@hotmail.com>
2018-02-18 23:37:37 -0300

    Comentarios y estilos google

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,29 @@
+/* Iniciando contador de respuestas correctas */
 let counterCorrect = 0;
+/* Iniciando contador de respuestas correctas */
 let counterIncorrect = 0;
 
+/* Evento de click para comenzar la trivia y para pasar a la siguiente pregunta*/
 $('#start, #next').click(function() {
-  fetch(`https://opentdb.com/api.php?amount=30&category=17&difficulty=easy&type=multiple`)
-
+  // Llamado a la api
+  fetch('https://opentdb.com/api.php?amount=30&category=17&difficulty=easy&type=multiple')
+    // Solicitando datos en archivo json
     .then(response => response.json())
+    // Obteniendo los datos
     .then(data => {
       $('#intro, .answer').remove();
       $('#trivias').empty();
       $('#next').removeAttr('class');
-
+      // Guardando los datos obtenidos
       const datos = data.results;
       const arrayDatos = datos.sort();
-
+      // Creando el DOM con los resultados obtenidos
       $('#trivias').append('<h3>' + arrayDatos[0].question + '</h3><button value="correct" class="option">' + arrayDatos[0].correct_answer + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[0] + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[1] + '</button><button value="incorrect" class="option">' + arrayDatos[0].incorrect_answers[2] + '</button>');
-      let sumCounter = counterCorrect+counterIncorrect;
+      // Guardando la suma de los resultados
+      let sumCounter = counterCorrect + counterIncorrect;
+      // Si la suma de los resultados es menos a 10 entonces puede continuar a la siguiente pregunta
       if (sumCounter < 10) {
-        // función seleccion
+        // función seleccion de respuesta
         $('.option').click(function() {
           const select = $(this).val();
           if (select === 'correct') {
@@ -29,10 +36,11 @@ $('#start, #next').click(function() {
             counterIncorrect++;
           }
         });
+        // Si la suma de los resultados es ihual a 10 entonces muestra los resultados.
       } if (sumCounter === 10) {
         $('#trivias').empty();
         $('#next').addClass('hidden');
-        $('#trivias').append('<h3 class="end"> You got ' + counterCorrect + ' correct answers and ' + counterIncorrect + ' incorrect answers. </h3>')
+        $('#trivias').append('<h3 class="end"> You got ' + counterCorrect + ' correct answers and ' + counterIncorrect + ' incorrect answers. </h3>');
       }
     });// data
 });
